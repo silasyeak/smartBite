2af4f8d2dae6a9bf442ff7b7ac6b9f82bee9d77c
tan29121996 <tan29121996@gmail.com>
2023-09-27 23:41:43 +0800

    Created functions to calculate health score for high cholesterol and pcos

--- components/AnalysisPage.js
+++ components/AnalysisPage.js
@@ -18,8 +18,8 @@ const AnalysisPage = () => {
   };
 
   const highBloodPressure = true;
-  const highCholesterol = true;
-  const pcos = true;
+  const highCholesterol = false;
+  const pcos = false;
   const weight = 'underweight';
 
   const calculateHealthScore = () => {
@@ -37,13 +37,13 @@ const AnalysisPage = () => {
       vitaminD,
     } = nutritionData;
 
-    totalFatPercent = (totalFat * 9)/calories;
-    saturatedFatPercent = (saturatedFat * 9)/calories;
-    cholesterolPercent = cholesterol/3;
-    sodiumPercent = sodium/23;
-    totalCarbohydratesPercent = (totalCarbohydrates * 4)/calories;
-    sugarsPercent = (sugars * 4)/calories;
-    proteinPercent = (protein * 4)/calories;
+    totalFatPercent = (totalFat * 9) / calories;
+    saturatedFatPercent = (saturatedFat * 9) / calories;
+    cholesterolPercent = cholesterol / 3;
+    sodiumPercent = sodium / 23;
+    totalCarbohydratesPercent = (totalCarbohydrates * 4) / calories;
+    sugarsPercent = (sugars * 4) / calories;
+    proteinPercent = (protein * 4) / calories;
 
     let score = 0;
 
@@ -55,6 +55,17 @@ const AnalysisPage = () => {
         if (cholesterolPercent <= 0.05) score += 1;
         if (sodiumPercent <= 0.06) score += 1;
         if (totalCarbohydratesPercent >= 0.5 && totalCarbohydratesPercent <= 0.6) score += 1;
+        if (dietaryFiber >= 0.25) score += 1;
+        if (sugarsPercent <= 0.1) score += 1;
+        if (proteinPercent >= 0.1 && protein <= 0.2) score += 1;
+        if (vitaminD) score += 1;
+      } else if (weight == 'normal') {
+        if (totalFatPercent >= 0.15 && totalFat <= 0.2) score += 1;
+        if (saturatedFatPercent <= 0.05) score += 1;
+        if (transFat === 0) score += 1;
+        if (cholesterolPercent <= 0.05) score += 1;
+        if (sodiumPercent <= 0.06) score += 1;
+        if (totalCarbohydratesPercent >= 0.5 && totalCarbohydrates <= 0.6) score += 1;
         if (dietaryFiber >= 0.25) score += 1;
         if (sugarsPercent <= 0.1) score += 1;
         if (proteinPercent >= 0.1 && protein <= 0.2) score += 1;
@@ -81,16 +92,101 @@ const AnalysisPage = () => {
         if (sugarsPercent <= 0.06) score += 1;
         if (proteinPercent >= 0.15 && protein <= 0.2) score += 1;
         if (vitaminD) score += 1;
-      } else {
-        if (totalFatPercent >= 0.15 && totalFat <= 0.2) score += 1;
-        if (saturatedFatPercent <= 0.05) score += 1;
+      }
+    }
+
+    if (highCholesterol) {
+      if (weight == 'underweight') {
+        if (totalFatPercent <= 0.25) score += 1;
+        if (saturatedFatPercent <= 0.07) score += 1;
         if (transFat === 0) score += 1;
         if (cholesterolPercent <= 0.05) score += 1;
-        if (sodiumPercent <= 0.06) score += 1;
+        if (sodiumPercent <= 0.07) score += 1;
+        if (totalCarbohydratesPercent >= 0.5 && totalCarbohydratesPercent <= 0.65) score += 1;
+        if (dietaryFiber >= 0.25) score += 1;
+        if (sugarsPercent <= 0.1) score += 1;
+        if (proteinPercent >= 0.12 && protein <= 0.2) score += 1;
+        if (vitaminD) score += 1;
+      } else if (weight == 'normal') {
+        if (totalFatPercent >= 0.2 && totalFat <= 0.25) score += 1;
+        if (saturatedFatPercent <= 0.07) score += 1;
+        if (transFat === 0) score += 1;
+        if (cholesterolPercent <= 0.05) score += 1;
+        if (sodiumPercent <= 0.07) score += 1;
         if (totalCarbohydratesPercent >= 0.5 && totalCarbohydrates <= 0.6) score += 1;
         if (dietaryFiber >= 0.25) score += 1;
         if (sugarsPercent <= 0.1) score += 1;
         if (proteinPercent >= 0.1 && protein <= 0.2) score += 1;
+        if (vitaminD) score += 1;
+      } else if (weight == 'overweight') {
+        if (totalFatPercent >= 0.15 && totalFat <= 0.2) score += 1;
+        if (saturatedFatPercent <= 0.07) score += 1;
+        if (transFat === 0) score += 1;
+        if (cholesterolPercent <= 0.05) score += 1;
+        if (sodiumPercent <= 0.07) score += 1;
+        if (totalCarbohydratesPercent >= 0.45 && totalCarbohydrates <= 0.55) score += 1;
+        if (dietaryFiber >= 0.25) score += 1;
+        if (sugarsPercent <= 0.08) score += 1;
+        if (proteinPercent >= 0.15 && protein <= 0.2) score += 1;
+        if (vitaminD) score += 1;
+      } else if (weight == 'obese') {
+        if (totalFatPercent <= 0.15) score += 1;
+        if (saturatedFatPercent <= 0.07) score += 1;
+        if (transFat === 0) score += 1;
+        if (cholesterolPercent <= 0.05) score += 1;
+        if (sodiumPercent <= 0.07) score += 1;
+        if (totalCarbohydratesPercent >= 0.4 && totalCarbohydrates <= 0.5) score += 1;
+        if (dietaryFiber >= 0.25) score += 1;
+        if (sugarsPercent <= 0.06) score += 1;
+        if (proteinPercent >= 0.15 && protein <= 0.2) score += 1;
+        if (vitaminD) score += 1;
+      }
+    }
+
+    if (pcos) {
+      if (weight == 'underweight') {
+        if (totalFatPercent <= 0.3) score += 1;
+        if (saturatedFatPercent <= 0.07) score += 1;
+        if (transFat === 0) score += 1;
+        if (cholesterolPercent <= 0.05) score += 1;
+        if (sodiumPercent <= 0.07) score += 1;
+        if (totalCarbohydratesPercent >= 0.55 && totalCarbohydratesPercent <= 0.65) score += 1;
+        if (dietaryFiber >= 0.25) score += 1;
+        if (sugarsPercent <= 0.1) score += 1;
+        if (proteinPercent >= 0.12 && protein <= 0.2) score += 1;
+        if (vitaminD) score += 1;
+      } else if (weight == 'normal') {
+        if (totalFatPercent >= 0.25 && totalFat <= 0.3) score += 1;
+        if (saturatedFatPercent <= 0.07) score += 1;
+        if (transFat === 0) score += 1;
+        if (cholesterolPercent <= 0.05) score += 1;
+        if (sodiumPercent <= 0.07) score += 1;
+        if (totalCarbohydratesPercent >= 0.5 && totalCarbohydrates <= 0.6) score += 1;
+        if (dietaryFiber >= 0.25) score += 1;
+        if (sugarsPercent <= 0.1) score += 1;
+        if (proteinPercent >= 0.15 && protein <= 0.2) score += 1;
+        if (vitaminD) score += 1;
+      } else if (weight == 'overweight') {
+        if (totalFatPercent >= 0.2 && totalFat <= 0.25) score += 1;
+        if (saturatedFatPercent <= 0.07) score += 1;
+        if (transFat === 0) score += 1;
+        if (cholesterolPercent <= 0.05) score += 1;
+        if (sodiumPercent <= 0.07) score += 1;
+        if (totalCarbohydratesPercent >= 0.45 && totalCarbohydrates <= 0.55) score += 1;
+        if (dietaryFiber >= 0.25) score += 1;
+        if (sugarsPercent <= 0.08) score += 1;
+        if (proteinPercent >= 0.15 && protein <= 0.2) score += 1;
+        if (vitaminD) score += 1;
+      } else if (weight == 'obese') {
+        if (totalFatPercent <= 0.2) score += 1;
+        if (saturatedFatPercent <= 0.07) score += 1;
+        if (transFat === 0) score += 1;
+        if (cholesterolPercent <= 0.05) score += 1;
+        if (sodiumPercent <= 0.07) score += 1;
+        if (totalCarbohydratesPercent >= 0.4 && totalCarbohydrates <= 0.5) score += 1;
+        if (dietaryFiber >= 0.25) score += 1;
+        if (sugarsPercent <= 0.06) score += 1;
+        if (proteinPercent >= 0.15 && protein <= 0.2) score += 1;
         if (vitaminD) score += 1;
       }
     }
@@ -123,7 +219,7 @@ const styles = StyleSheet.create({
   },
   healthScore: {
     fontSize: 18,
-    marginTop: 20, 
+    marginTop: 20,
   },
 });
 
